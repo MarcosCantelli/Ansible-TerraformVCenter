a4b832d88a37cb1f99c9b1e8de221ce3c99d8ab0
MrHighTech20 <marcoscantelli@hotmail.com>
2025-09-30 17:13:32 -0300

    criando main e variaveis para criação de VM baseado em template

---
+++ CreateVM_Terraform/variables.tf
@@ -0,0 +1,37 @@
+variable "vsphere_user" {
+  description = "Usuário para autenticação no vSphere"
+  type        = string
+}
+
+variable "vsphere_password" {
+  description = "Senha para autenticação no vSphere"
+  type        = string
+  sensitive   = true # Marked as sensitive to hide it in logs
+}
+
+variable "vsphere_server" {
+  description = "Endereço do servidor vSphere"
+  type        = string
+}
+
+
+variable "template_name" {
+  description = "Nome do Template"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Nome da Máquina Virtual"
+  type        = string
+}
+
+variable "num_cpus" {
+  description = "Número de CPUs"
+  type        = number
+}
+
+variable "memory_mb" {
+  description = "Memória em MB"
+  type        = number
+}
+
